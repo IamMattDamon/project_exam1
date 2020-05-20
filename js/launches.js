79d3fe8b2a23331bbd6683b454cc4f84ca6b94a1
IamMattDamon <frochi@gmail.com>
2020-05-20 19:09:43 +0200

    upcoming launches section complete

--- js/launches.js
+++ js/launches.js
@@ -1,3 +1,5 @@
+// Project Exam 1 - Launches.js
+
 const launchesUrl = "https://api.spacexdata.com/v3/launches";
 const nextLaunchUrl = "https://api.spacexdata.com/v3/launches/next";
 const upcomingLaunchUrl = "https://api.spacexdata.com/v3/launches/upcoming";
@@ -8,33 +10,33 @@ async function fetchAPI() {
     try {
     const nextLaunchResp = await fetch(nextLaunchUrl);
     const nextLaunch = await nextLaunchResp.json();
-    displayNextLaunch(nextLaunch);
-    displayCountdown(nextLaunch);
+    arrayNextLaunch(nextLaunch);
+    arrayCountdown(nextLaunch);
 
     const upcomingLaunchResp = await fetch(upcomingLaunchUrl);
     const upcomingLaunches = await upcomingLaunchResp.json();
-    displayUpcomingLaunches(upcomingLaunches);
+    arrayUpcomingLaunches(upcomingLaunches);
 
     const pastLaunchResp = await fetch(pastLaunchUrl);
     const pastLaunch = await pastLaunchResp.json();
-    displayPastLaunch(pastLaunch);
-    }
-    catch (error) {
+    arrayPastLaunches(pastLaunch);
+    
+    }   catch (error) {
         console.log(error);
     }
 }
 
 fetchAPI();
 
-// Convert date formatting (from same source as countdown)
+// Convert date formatting (code from same source as the countdown)
 function convertDate(launchDate) {
     let date = new Date(launchDate * 1000);
     return date.toLocaleDateString();
 }
 
-// Next launch
-function displayNextLaunch(nextLaunch) {
-    const nextLaunchQuery = document.querySelector(".next-launch");
+// Next launch section
+function arrayNextLaunch(nextLaunch) {
+    const nextLaunchContainer = document.querySelector(".next-launch");
     
     let html = "";
     
@@ -75,10 +77,88 @@ function displayNextLaunch(nextLaunch) {
                 </div>
                 `;
 
-    nextLaunchQuery.innerHTML = html;
+    nextLaunchContainer.innerHTML = html;
 
     }
-    
+
+    // Countdown to next launch (Not my own code - references)
+    function arrayCountdown(data) {
+        setInterval(function() {
+            let distance, weeks, days, hours, minutes, seconds;
+            const now = Math.floor(new Date().getTime());
+        
+            distance = data.launch_date_unix*1000 - now;
+            weeks = Math.floor(distance / 604800000);
+            days = Math.floor(distance / (1000 * 60 * 60 * 24)/7);
+            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            if (distance > 0) {
+                document.getElementById('weeks').innerHTML = weeks;
+                document.getElementById('days').innerHTML = days;
+                document.getElementById('hours').innerHTML = hours;
+                document.getElementById('minutes').innerHTML = minutes;
+                document.getElementById('seconds').innerHTML = seconds;   
+            } else if (data.tbd === true) {
+                document.getElementById('countdown').innerHTML = `<h3>Launch is delayed</h3>
+                                                                 <h3>New date to be determined</h3>
+                                                                 `;
+            } else {
+                document.getElementById('countdown').innerHTML = `<h3>Launch in progress</h3>`;
+            }
+        }, 1000
+        ) 
+        
+        }
+        
+        // Upcoming launches section
+        function arrayUpcomingLaunches(upcomingLaunches) {
+            const upcomingLaunchesContainer = document.querySelector(".upcoming-launches");
+
+            let html = "";
+
+            for (let i = 1; i < upcomingLaunches.length; i++) {
+                html += `
+                        <div class="upcoming-container">
+                        <input type="checkbox" id="ul_toggle${i}" class="toggle"/>
+                        <label class="dropdown-btn" for="ul_toggle${i}">
+                            <h3 class="dropdown-btn centered">${upcomingLaunches[i].mission_name}</h3>
+                        </label>
+                            <div class="dropdown-content">
+                                <div class="upcoming-data">
+                                    <h4 class="launch-header-small">DATE</h4>
+                                        <hr class="hr-launch">
+                                        <p class="launch-details">${convertDate(upcomingLaunches[i].launch_date_unix)}</p>
+                                    <h4 class="launch-header-small">ROCKET</h4>
+                                        <hr class="hr-launch">
+                                        <p class="launch-details">${upcomingLaunches[i].rocket.rocket_name}</p>
+                                    <h4 class="launch-header-small">LAUNCH SITE</h4>
+                                        <hr class="hr-launch">
+                                        <p class="launch-details">${upcomingLaunches[i].launch_site.site_name_long}</p>
+                                    <h4 class="launch-header-small">DETAILS</h4>
+                                        <hr class="hr-launch">                                      
+                            `;
+
+                if (!upcomingLaunches[i].details) {
+                    html += `<p class="launch-details">No details available for this mission</p>`
+                } 
+                else {
+                    html += `<p class="launch-details">${upcomingLaunches[i].details}</p>`
+                }
+            
+                html += `</div>
+                         </div>
+                         </div>
+                        `;    
+            }
+
+            upcomingLaunchesContainer.innerHTML = html;
+            
+        }
+
+// Past launches section
+
 
 
 
